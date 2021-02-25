9ce3983e9c9baec2b7ca02d52687fb467cf0f517
Ryan Clements <ryanclementshax@gmail.com>
2021-02-24 21:42:49 -0500

    weapon detail

---
+++ GW2Api.NET/V1/Items/Dto/ItemTypes/Weapon/DamageType.cs
@@ -0,0 +1,11 @@
+﻿namespace GW2Api.NET.V1.Items.Dto.ItemTypes.Weapon
+{
+    public enum DamageType
+    {
+        Choking,
+        Fire,
+        Ice,
+        Lightning,
+        Physical
+    }
+}
--- GW2Api.NET/V1/Items/Dto/ItemTypes/Weapon/WeaponSubDetail.cs
+++ GW2Api.NET/V1/Items/Dto/ItemTypes/Weapon/WeaponSubDetail.cs
@@ -1,18 +1,19 @@
 ﻿using GW2Api.NET.Json.Converters;
+using GW2Api.NET.V1.Items.Dto.ItemTypes.Common;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace GW2Api.NET.V1.Items.Dto.ItemTypes.Weapon
 {
     public record WeaponSubDetail(
-        string Type,
-        string DamageType,
+        WeaponType Type,
+        DamageType DamageType,
         [property: JsonConverter(typeof(StringToIntConverter))] int MinPower,
         [property: JsonConverter(typeof(StringToIntConverter))] int MaxPower,
         [property: JsonConverter(typeof(StringToIntConverter))] int Defense,
-        IReadOnlyCollection<string> InfusionSlots,
+        IReadOnlyCollection<InfusionType> InfusionSlots,
         double AttributeAdjustment,
-        object InfixUpgrade,
+        InfixUpgrade InfixUpgrade,
         [property: JsonConverter(typeof(StringToNullableIntConverter))] int? SuffixItemId,
         [property: JsonConverter(typeof(StringToNullableIntConverter))] int? SecondarySuffixItemId
     );
